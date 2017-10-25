a808ab6415a08d3eb5d967ab8ce1c961b34aac65
tammy le <tammyle@tammys-MBP.attlocal.net>
2017-10-24 23:00:14 -0700

    scrape sucessful

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 // Dependencies
 
-var express = require("express")
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
 var mongoose = require("mongoose");
-var bodyParser = require("body-parser")
-var logger = require("morgan")
-
-//require Note.js and Article.js
-var Note = require("./models/Note.js")
-var Article = require("./models/Article.js")
-
-// Scrapping tools
-var cheerio = require("cheerio")
-var request = require("request")
-
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
+
 
 // Initialize Express
 var app = express();
